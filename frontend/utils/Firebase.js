9b344bd388207eac0137a8231883342bf05feaa8
pratikp23 <pratikpathak2308@gmail.com>
2026-05-11 22:10:40 +0530

    remove firebase analyst

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -1,5 +1,5 @@
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyB1pIGDYK2CWRWKAG4eAJhSSi75Bu4eXbg",
   authDomain: "learning-mananagement-sy-17d27.firebaseapp.com",
@@ -9,8 +9,6 @@ const firebaseConfig = {
   appId: "1:694813844924:web:5b5ef2f782f2bf45a437b9"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-const provider = new GoogleAuthProvider()
-export {auth,provider}
+
+export default app;
