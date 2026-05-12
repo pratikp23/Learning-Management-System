a0ad89cc499fb5d7e93cafe6e0ca7f75243e5c65
pratikp23 <pratikpathak2308@gmail.com>
2026-05-12 08:43:39 +0530

    fix firebase env variables issue

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -2,10 +2,10 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1pIGDYK2CWRWKAG4eAJhSSi75Bu4eXbg",
-  authDomain: "learning-mananagement-sy-17d27.firebaseapp.com",
-  projectId: "learning-mananagement-sy-17d27",
-  storageBucket: "learning-mananagement-sy-17d27.firebasestorage.app",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: "694813844924",
   appId: "1:694813844924:web:5b5ef2f782f2bf45a437b9"
 };
