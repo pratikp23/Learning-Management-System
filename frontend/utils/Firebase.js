479151a7b7a897f4d62c54ddc3f116c0e1c66594
pratikp23 <pratikpathak2308@gmail.com>
2026-05-12 09:23:20 +0530

    fix firebase initialization issue

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -1,16 +1,16 @@
-console.log("API KEY =", import.meta.env.VITE_FIREBASE_API_KEY);
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  apiKey: "AIzaSyB1pIGDYK2CWRWKAG4eAJhSSi75Bu4eXbg",
+  authDomain: "learning-mananagement-sy-17d27.firebaseapp.com",
+  projectId: "learning-mananagement-sy-17d27",
+  storageBucket: "learning-mananagement-sy-17d27.firebasestorage.app",
   messagingSenderId: "694813844924",
   appId: "1:694813844924:web:5b5ef2f782f2bf45a437b9"
 };
 
+// IMPORTANT: prevent multiple init
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const auth = getAuth(app);
